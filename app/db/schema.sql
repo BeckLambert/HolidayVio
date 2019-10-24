9ae28f8c98912904c34b9fb34e376acae7ba4b4c
Zach Christie <zachchristieofficial@gmail.com>
2019-10-24 09:23:34 -0600

    Updated

--- app/db/schema.sql
+++ app/db/schema.sql
@@ -4,11 +4,5 @@ CREATE DATABASE holiday_db;
 
 USE holiday_db;
 
-CREATE TABLE quiz (
-    id AUTO_INCREMENT NOT NULL PRIMARY KEY,
-    question VARCHAR(244) NOT NULL, 
-    userAns VARCHAR (244) NULL
-)
-
 
 
